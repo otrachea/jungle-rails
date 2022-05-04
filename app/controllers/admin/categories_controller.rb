fd15a8c468bc1a1d94b6c33de45e58bddcbaf803
otrachea <francesca.ho459@gmail.com>
2022-05-04 22:23:07 +0000

    Admin category page require user/password and show categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,3 +1,29 @@
 class Admin::CategoriesController < ApplicationController
-  
+  http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASSWORD"], except: :inde
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(cateogry_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cateogry_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
 end
