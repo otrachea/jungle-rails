242a92eeb57b8b354f6b2b820ee242e5fb072b6f
otrachea <francesca.ho459@gmail.com>
2022-05-04 20:42:45 +0000

    Dashboard shows table for number of categories and unique plants, cat category table shows categories and how many plants of each type and table for all plants

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Admin::DashboardController < ApplicationController
   def show
     @products = Product.all
-    # @category = Category.all
+    @categories = Category.all.map do |category|
+      {name: category.name, num_of_unique_plants: Product.where(category_id: category.id).count}
+    end
+    @plant_total = Product.count
+    @category_total = Category.count
   end
 end
