7bfe38c84d0250f51335961da627a176628d4c83
otrachea <francesca.ho459@gmail.com>
2022-05-05 19:39:39 +0000

    Add first and last name to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :email, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :password, :email, :password_confirmation)
   end
 
 end
