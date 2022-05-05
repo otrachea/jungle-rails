d14b8a8c4a06c14aefd6756dfa58724e3dee508f
otrachea <francesca.ho459@gmail.com>
2022-05-05 19:03:46 +0000

    User can signup, login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show], skip: [:delete]
   end
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
 
