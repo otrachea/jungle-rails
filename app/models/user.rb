eea43deb3e98f1a7d48464c12952c273c183c03b
otrachea <francesca.ho459@gmail.com>
2022-05-09 16:47:46 +0000

    Add validation for email, first name, last name in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
 
   has_secure_password
   
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
   
