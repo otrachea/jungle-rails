62fd29a91b54c1b23aa03e2bc6eb3c31d2340cd1
otrachea <francesca.ho459@gmail.com>
2022-05-10 12:33:04 +0000

    Finish testing home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,15 +1,19 @@
 describe("jungle", () => {
-
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
-  })
+  });
 
   it("visits the homepage by default", () => {
-    cy.get(".intro").contains("Looking for a way to add some life to your home? Look no further than our online plant shop! We have a wide variety of beautiful plants and flowers, as well as all the accessories you need to keep them healthy and happy. Plus, our prices are unbeatable! So what are you waiting for?")
-  })
+    cy.get(".intro").contains(
+      "Looking for a way to add some life to your home? Look no further than our online plant shop! We have a wide variety of beautiful plants and flowers, as well as all the accessories you need to keep them healthy and happy. Plus, our prices are unbeatable! So what are you waiting for?"
+    );
+  });
 
-  it("There is products on the page", () => {
+  it("There are products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
-  
-})
+
+  it("There are 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+});
