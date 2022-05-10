23cd03023ec1ff4b0a7a4779f9ab5cecb141f8e0
otrachea <francesca.ho459@gmail.com>
2022-05-10 12:39:29 +0000

    Add cypress test to check if clicking on product on home page redirects to product's page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,11 @@
+describe("jungle", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.get(".products article").first().click();
+  });
+
+  it("visits the product after clicking on it", () => {
+    cy.get("section.products-show")
+  });
+
+});
